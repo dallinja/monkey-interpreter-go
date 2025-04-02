733cd6af75512183e94394585956ec15d9ba287f
Dallin Anderson <dallinja@gmail.com>
2025-04-01 21:51:55 -0600

    Add return statements evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,7 +17,7 @@ func Eval(node ast.Node) object.Object {
 	// Statements
 	case *ast.Program:
 		// fmt.Println("*ast.Program")
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 
 	case *ast.ExpressionStatement:
 		// fmt.Println("*ast.ExpressionStatement")
@@ -52,20 +52,42 @@ func Eval(node ast.Node) object.Object {
 		return v
 
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlackStatement(node)
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlackStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
 	}
 
 	return result
